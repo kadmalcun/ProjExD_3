c74c2b3efee7ac88291234861042ac5b475a201d
表野楓 <kaede@hyouyafuunoMacBook-Pro.local>
2025-12-02 14:32:17 +0900

    練習3:喜びエフェクト

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -173,6 +173,8 @@ def main():
                     # ビームが爆弾に当たったらビームと爆弾を消す
                     beam = None
                     bomb = None
+                    bird.change_img(6, screen)
+                    pg.display.update()
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
